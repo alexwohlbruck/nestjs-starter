008a647487e15ec22b2059a6b2ad485ee307249d
Alex Wohlbruck <alexwohlbruck@gmail.com>
2022-06-20 19:01:58 -0400

    Cleanup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,8 @@
-import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common'
-import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { Controller, Get, Query, Request } from '@nestjs/common'
 import { Prisma, Role, User as UserModel } from '@prisma/client'
 import { UsersService } from './users.service'
 import { ApiTags } from '@nestjs/swagger'
 import { Roles } from '../auth/auth-decorators'
-import { RolesGuard } from '../auth/roles.guard'
 import { JwtPayload } from '../auth/jwt.strategy'
 
 @ApiTags('Users')
